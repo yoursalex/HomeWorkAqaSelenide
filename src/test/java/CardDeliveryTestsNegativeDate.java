b00feb8d2c4988b4aa9e9c7493861a633225ce76
yoursalex <sash34@mail.ru>
2019-11-01 21:34:57 +0300

    Tests added

---
+++ src/test/java/CardDeliveryTestsNegativeDate.java
@@ -0,0 +1,46 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class CardDeliveryTestsNegativeDate {
+
+    @Test
+    @DisplayName("Не должен бронировать карту если дата меньше 3 дней с текущей")
+
+    void shouldNotSubmitWithWrongDate() {
+        LocalDate today = LocalDate.now();
+        LocalDate dateToBeSet = today.plusDays(2);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String stringDate = dateToBeSet.format(formatter);
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input.input__control").setValue("Москва");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $("[data-test-id=date] input.input__control").setValue(stringDate);
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=date] .input").shouldHave(Condition.cssClass("input_invalid"));
+    }
+
+    @Test
+    @DisplayName("Не должен бронировать карту если дата заказа не указана")
+
+    void shouldNotSubmitWithEmptyDate() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input.input__control").setValue("Москва");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=date] .input").shouldHave(Condition.cssClass("input_invalid"));
+    }
+
+}
---
+++ src/test/java/CardDeliveryTestsPositiveDate.java
@@ -0,0 +1,56 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.Keys;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class CardDeliveryTestsPositiveDate {
+
+    private String stringDate;
+
+    @BeforeEach
+    void setUp() {
+        LocalDate today = LocalDate.now();
+        LocalDate dateToBeSet = today.plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        stringDate = dateToBeSet.format(formatter);
+    }
+
+    @Test
+    @DisplayName("Должен бронировать карту с доставкой при вводе валидных данных")
+
+    void shouldSubmitRequest() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input.input__control").setValue("Москва");
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $("[data-test-id=date] input.input__control").setValue(stringDate);
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Успешно!")).waitUntil(Condition.visible, 15000);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/dataOptions.cvs", numLinesToSkip = 1)
+    @DisplayName("Не должен бронировать доставку при невалидных данных")
+    void shouldNotSubmitRequest(String city, String name, String phone) {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input.input__control").setValue(city);
+        $("[data-test-id=date] input.input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        $("[data-test-id=date] input.input__control").setValue(stringDate);
+        $("[data-test-id=name] input.input__control").setValue(name);
+        $("[data-test-id=phone] input.input__control").setValue(phone);
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("fieldset").$("span").shouldHave(Condition.cssClass("input_invalid")); //здесь выходит, что работает только для первого набора данных..
+    }
+
+}
